927bd523c6e195805aa97293af1598c2c1f143a5
Nikita <fidg37@gmail.com>
2020-08-12 15:05:22 +0200

    каррирование 2

--- src/loadButtonLogic.js
+++ src/loadButtonLogic.js
@@ -3,25 +3,21 @@ import {render} from "./util";
 import {createTaskCardTemplate} from "./View/task-card";
 import {createLoadButtonTemplate} from "./View/load-button.js";
 
-const loadButtonClickEvent = (tasks) => (
-  (taskListContainer) => (
-    (button) => (
-      (evt) => {
-        evt.preventDefault();
+const loadButtonClickEvent = (tasks, taskListContainer, button) => (evt) => {
+  evt.preventDefault();
 
-        const renderedTasksCount = taskListContainer.querySelectorAll(`.card__form`).length;
-        if (renderedTasksCount >= tasks.length) {
-          button.removeEventListener(`click`, loadButtonClickEvent(tasks)(taskListContainer)(button));
-          button.remove();
-        } else {
-          tasks
-        .slice(renderedTasksCount, renderedTasksCount + IterationCount.MAX_CARD_PER_STEP)
-        .forEach((task) => render({container: taskListContainer, template: createTaskCardTemplate(task)}));
-        }
-      }
-    )
-  )
-);
+  const renderedTasksCount = taskListContainer.querySelectorAll(`.card__form`).length;
+
+  tasks
+    .slice(renderedTasksCount, renderedTasksCount + IterationCount.MAX_CARD_PER_STEP)
+    .forEach((task) => render({container: taskListContainer, template: createTaskCardTemplate(task)}));
+
+  if (renderedTasksCount + IterationCount.MAX_CARD_PER_STEP >= tasks.length) {
+    button.removeEventListener(`click`, loadButtonClickEvent(tasks, taskListContainer, button));
+    //button.remove();
+    console.log(`EVENT!`);
+  }
+};
 
 export const createLoadButton = (tasks, buttonContainer, taskListContainer) => {
   if (tasks.length > IterationCount.MAX_CARD_PER_STEP) {
@@ -29,6 +25,6 @@ export const createLoadButton = (tasks, buttonContainer, taskListContainer) => {
 
     const button = buttonContainer.querySelector(`.load-more`);
 
-    button.addEventListener(`click`, loadButtonClickEvent(tasks)(taskListContainer)(button));
+    button.addEventListener(`click`, loadButtonClickEvent(tasks, taskListContainer, button));
   }
 };
